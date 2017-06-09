e9ef0a5434ba9b06b119c56c6edacbdc49c34e91
Ashwini <ashwinipkhilari28@gmail.com>
2017-06-09 15:13:21 +0530

    first change

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -3,10 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" system out for git changess");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
